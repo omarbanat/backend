1fc472afd410ba1044298965bc6a205b236b4e6c
Omar Banat <o.j.banat@gmail.com>
2023-09-22 10:57:46 +0300

    Modify in Get Post Delete APIs

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 const port = 8000;
 const MONGODB_URL = process.env.MONGODB_URL;
 
+// used for sending json body in the request
+app.use(express.json());
+app.use(express.urlencoded());
+
 async function main() {
   await mongoose.connect(MONGODB_URL);
 }
@@ -27,7 +31,7 @@ main()
 // mainUsingTryCatch();
 
 const blogsSchema = new Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: true, unique: true },
   publisher: { type: String, required: true },
   date: { type: Date, required: true },
   body: { type: String, required: true },
@@ -37,35 +41,58 @@ const blogsSchema = new Schema({
 const Blog = model('Blogs', blogsSchema);
 
 app.get('/', async (req, res) => {
-  const blogs = await Blog.find({});
-  res.status(200).json({
-    success: true,
-    message: 'Data retrieved successfully',
-    data: blogs,
-  });
+  try {
+    const blogs = await Blog.find({});
+    res.status(200).json({
+      success: true,
+      message: 'Data retrieved successfully',
+      data: blogs,
+    });
+  } catch (error) {
+    res.status(200).json({
+      success: false,
+      message: 'unable to get data',
+      error: error,
+    });
+  }
 });
 
 app.post('/', async (req, res) => {
-  const blog = await Blog.create({
-    title: 'Title2',
-    publisher: 'Publisher2',
-    date: '2023-09-22',
-    body: 'Any text2',
-    keyword: 'body2',
-  });
-  res.status(200).json({
-    success: true,
-    message: 'Blog added successfully',
-    data: blog,
-  });
+  try {
+    const blog = await Blog.create(req.body);
+    res.status(200).json({
+      success: true,
+      message: 'Blog added successfully',
+      data: blog,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Blog not added successfully',
+      error: error,
+    });
+  }
 });
 
 app.put('/', (req, res) => {
   //   res.send('Hello from /put request');
 });
 
-app.delete('/', (req, res) => {
-  //   res.send('Hello from /delete request');
+app.delete('/:ID', async (req, res) => {
+  try {
+    const blog = await Blog.deleteOne({ _id: req.params.ID });
+    res.status(200).json({
+      success: true,
+      message: 'Blog deleted successfully',
+      blog: blog,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Error occured while deleting the blog',
+      error: error,
+    });
+  }
 });
 
 app.listen(port, () => {
