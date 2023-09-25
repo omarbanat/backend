f6b71e64df8cdbc4d166006f35ae1f40a8feebb3
Omar Banat <o.j.banat@gmail.com>
2023-09-25 12:42:58 +0300

    MVC Routes

---
+++ routes/blogsRoute.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const router = express.Router();
+const Blog = require('../models/Blog');
+
+router.get('/getAll', async (req, res) => {
+  try {
+    const blogs = await Blog.find({});
+    res.status(200).json({
+      success: true,
+      message: 'Data retrieved successfully',
+      data: blogs,
+    });
+  } catch (error) {
+    res.status(200).json({
+      success: false,
+      message: 'unable to get data',
+      error: error,
+    });
+  }
+});
+
+router.get('/get/:ID', async (req, res) => {
+  try {
+    // const blog = await Blog.find({ _id: req.params.ID });
+    const blog = await Blog.findById(req.params.ID);
+    res.status(200).json({
+      success: true,
+      message: 'Blog retrieved successfully',
+      data: blog,
+    });
+  } catch (error) {
+    res.status(200).json({
+      success: false,
+      message: 'unable to get blog by ID',
+      error: error,
+    });
+  }
+});
+
+router.post('/add', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    res.status(200).json({
+      success: true,
+      message: 'Blog added successfully',
+      data: blog,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Blog not added successfully',
+      error: error,
+    });
+  }
+});
+
+router.put('/update/:ID', async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndUpdate(req.params.ID, req.body);
+    res.status(200).json({
+      success: true,
+      message: 'Blog updated successfully.',
+      data: blog,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Unable to update blog',
+      error: error,
+    });
+  }
+});
+
+router.delete('/delete/:ID', async (req, res) => {
+  try {
+    const blog = await Blog.deleteOne({ _id: req.params.ID });
+    res.status(200).json({
+      success: true,
+      message: 'Blog deleted successfully',
+      blog: blog,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Error occured while deleting the blog',
+      error: error,
+    });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const dbConnection = require('./config/db');
-const Blog = require('./models/Blog');
+const blogRoutes = require('./routes/blogsRoute');
 
 const app = express();
 const port = 8000;
@@ -10,91 +10,7 @@ const port = 8000;
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/', async (req, res) => {
-  try {
-    const blogs = await Blog.find({});
-    res.status(200).json({
-      success: true,
-      message: 'Data retrieved successfully',
-      data: blogs,
-    });
-  } catch (error) {
-    res.status(200).json({
-      success: false,
-      message: 'unable to get data',
-      error: error,
-    });
-  }
-});
-
-app.get('/:ID', async (req, res) => {
-  try {
-    // const blog = await Blog.find({ _id: req.params.ID });
-    const blog = await Blog.findById(req.params.ID);
-    res.status(200).json({
-      success: true,
-      message: 'Blog retrieved successfully',
-      data: blog,
-    });
-  } catch (error) {
-    res.status(200).json({
-      success: false,
-      message: 'unable to get blog by ID',
-      error: error,
-    });
-  }
-});
-
-app.post('/', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body);
-    res.status(200).json({
-      success: true,
-      message: 'Blog added successfully',
-      data: blog,
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: 'Blog not added successfully',
-      error: error,
-    });
-  }
-});
-
-app.put('/:ID', async (req, res) => {
-  try {
-    const blog = await Blog.findByIdAndUpdate(req.params.ID, req.body);
-    res.status(200).json({
-      success: true,
-      message: 'Blog updated successfully.',
-      data: blog,
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: 'Unable to update blog',
-      error: error,
-    });
-  }
-});
-
-app.delete('/:ID', async (req, res) => {
-  try {
-    const blog = await Blog.deleteOne({ _id: req.params.ID });
-    res.status(200).json({
-      success: true,
-      message: 'Blog deleted successfully',
-      blog: blog,
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: 'Error occured while deleting the blog',
-      error: error,
-    });
-  }
-});
+app.use('/blog', blogRoutes);
 
 app.listen(port, () => {
   dbConnection()
