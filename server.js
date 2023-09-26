45397437f6d430b3f4cc0a1e9da8297786166428
Omar Banat <o.j.banat@gmail.com>
2023-09-26 10:57:25 +0300

    Add cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const dbConnection = require('./config/db');
 const blogRoutes = require('./routes/blogsRoute');
 
 const app = express();
 const port = 8000;
+
+app.use(cors());
 
 // used for sending json body in the request
 app.use(express.json());
